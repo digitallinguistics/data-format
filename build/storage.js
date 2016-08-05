5c4606cf144e68205a7dbae31f4dace3e928e2b2
Daniel W. Hieber <dwhieb@users.noreply.github.com>
2016-08-05 03:46:47 -0500

    automated schema storage with Azure Blob Service (#19)
    * updates linter (JSHint > ESLint)
    * update URL for CDN
    * update `.gitignore`
    * rewrites `storage.js` to store updated schemas with Azure Blob Service
    * init `.travis.yml`
    * basic Travis config
    * add Azure blob storage to build process
    * update devDependencies
      * azure-storage@1.2.0
    * closes #20

--- build/storage.js
+++ build/storage.js
@@ -1,39 +1,137 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+const runGenerator = require('./runGenerator');
 const Storage = require('azure-storage');
-require('../../credentials/dlx-spec');
 
+if (process.env.NODE_ENV === 'local') {
+  require('../../credentials/dlx-spec');
+}
+
+const blobList = [];
 const storage = Storage.createBlobService();
+const uploadedStatus = 201;
 
-// Stores a file in Azure Blob Storage by running the following command:
-// `node build/storage.js store abbreviation.json`
-// This will parse the file `abbreviation.json` in the `/schemas` folder,
-//   read the version number from the `id` field of the schema,
-//   and upload a new blob whose filename includes the version number.
+const logError = err => console.error(err, err.stack);
 
-if (process.argv[2] === 'store') {
+runGenerator(function* main() {
 
-  const schema = require(`../schemas/${process.argv[3]}`);
-  const filename = schema.id.match(/\/schemas\/([^/]+\.json)/)[1];
-  const text = JSON.stringify(schema, null, 2);
+  const getBlobList = continuationToken => new Promise(resolve => {
+    storage.listBlobsSegmented('schemas', continuationToken, (err, res) => {
 
-  const opts = {
-    contentEncoding: 'utf-8',
-    contentType: 'application/schema+json'
-  };
+      if (err) {
+        logError(err);
+        throw new Error('Unable to list blobs');
+      }
 
-  storage.createBlockBlobFromText('schemas', filename, text, opts, (err) => {
-    if (err) { console.error(err); }
-    else {
+      const blobNames = res.entries.map(entry => entry.name);
+      blobList.push(...blobNames);
 
-      console.log('Current version stored successfully.');
+      if (res.continuationToken) {
+        getBlobList(res.continuationToken);
+      }
 
-      const filename = process.argv[3].replace(/\.json$/, '-latest.json');
+      resolve();
 
-      storage.createBlockBlobFromText('schemas', filename, text, opts, err => {
-        if (err) { console.error(err); }
-        else { console.log('Latest version stored successfully.'); }
-      });
-
-    }
+    });
   });
 
-}
+  const getFileList = () => new Promise(resolve => {
+    const filepath = path.join(__dirname, '../schemas');
+
+    fs.readdir(filepath, 'utf8', (err, filenames) => {
+      if (err) {
+        logError(err);
+        throw new Error('Unable to list files in /schemas folder.');
+      }
+      resolve(filenames);
+    });
+  });
+
+  const readSchema = filename => new Promise((resolve, reject) => {
+
+    const filepath = path.join(__dirname, '../schemas', filename);
+
+    fs.readFile(filepath, 'utf8', (err, text) => {
+      if (err) {
+        console.error(`Unable to read schema from ${filename}`);
+        reject(err);
+      }
+      resolve(text);
+    });
+
+  });
+
+  const updateSchema = filename => new Promise((resolve, reject) => {
+    runGenerator(function* update() {
+
+      let text;
+
+      try {
+        text = yield readSchema(filename).catch(logError);
+      } catch (err) {
+        console.error(`Error reading schema: ${filename}.`);
+        reject(err);
+      }
+
+      const schema = JSON.parse(text);
+      const versionedBlobName = schema.id.match(/\/schemas\/([^/]+\.json)/)[1];
+      const latestBlobName = filename.replace('.json', '-latest.json');
+      const blobExists = blobList.includes(versionedBlobName);
+
+      if (blobExists) {
+        console.log(`Schema ${versionedBlobName} up to date.`);
+      } else if (!blobExists) {
+
+        const schemaVersion = schema.id.match(/-([^/]+)\.json/)[1];
+
+        const blobStorageOpts = {
+          metadata: {
+            version: schemaVersion,
+          },
+          contentSettings: {
+            contentType:     'application/json',
+            contentEncoding: 'utf8',
+          },
+        };
+
+        try {
+          yield uploadSchema(versionedBlobName, blobStorageOpts, text).catch(logError);
+          yield uploadSchema(latestBlobName, blobStorageOpts, text).catch(logError);
+        } catch (err) {
+          console.error(`Error updating schema: ${filename}.`);
+          reject(err);
+        }
+
+      }
+
+    }, [filename]).catch(logError);
+  });
+
+  const uploadSchema = (blobName, opts, text) => new Promise((resolve, reject) => {
+    storage.createBlockBlobFromText('schemas', blobName, text, opts, (err, res, headers) => {
+
+      if (err) {
+        console.error(`Unable to upload schema for ${blobName}.`);
+        reject(err);
+      } else if (headers.statusCode != uploadedStatus) {
+        throw new Error(`Issue uploading schema for ${blobName}.`);
+      }
+
+      resolve();
+
+    });
+  });
+
+  const fileList = yield getFileList().catch(logError);
+
+  yield getBlobList().catch(logError);
+
+  yield Promise.all(fileList.map(updateSchema))
+    .then(() => console.log(`All schemas successfully updated.`))
+    .catch(err => {
+      console.error(`There was an error updating the schemas.`);
+      logError(err);
+    });
+
+}).catch(logError);
