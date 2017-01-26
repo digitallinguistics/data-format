e6a40501c3f4692bfe3ed21270f358060f93f9ac
Daniel W. Hieber <dwhieb@gmail.com>
2017-01-26 13:13:20 -0600

    Tests (#30)
    * install Jasmine
    
    * add test to validate schemas
    
    * all tests passing
    
    * add Travis config
    
    * update docs

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const AJV = require('ajv');
+const schemas = require('../schemas');
+
+const ajv = new AJV();
+
+describe('schemas: ', function() {
+
+  for (const schema in schemas) {
+
+    it(`${schema} is valid against JSON Schema Draft 4`, function() {
+
+      const valid = ajv.validateSchema(schemas[schema]);
+
+      if (valid) expect(valid).toBe(true);
+      else fail(ajv.errorsText());
+
+    });
+
+  }
+
+});
