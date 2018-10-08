cf4fa0c176974832eaa909001d048096249b9792
Daniel W. Hieber <dwhieb@gmail.com>
2018-10-08 14:35:22 -0500

    point to official GeoJSON schema
    closes #228

--- test/Location.test.js
+++ test/Location.test.js
@@ -9,6 +9,7 @@ let validate;
 const data = {
   dateCreated:  `2018-10-05T15:26:23.070Z`,
   dateModified: `2018-10-05T15:26:23.070Z`,
+  geoJSON:      {},
   id:           `46d3ad45-d1b8-4656-b496-dcb39203c5a9`,
   link:         `https://data.digitallinguistics.io/languages/Chitimacha/locations/Charenton`,
   name:         {
