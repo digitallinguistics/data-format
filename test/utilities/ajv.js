b029945bf8fe794bd9b1f276e88d5da9450882e0
Daniel W. Hieber <dwhieb@gmail.com>
2018-10-09 12:10:09 -0500

    JSON Schema Draft 07 (#96)
    * DEP: ajv@6.5.4
    * CHANGE: update schemas to JSON Schema Draft 07

--- test/utilities/ajv.js
+++ test/utilities/ajv.js
@@ -49,16 +49,16 @@ async function loadSchemas() {
   for (const [, schema] of schemas) {
 
     // Extract the ID from the "id" property of the schema
-    const IDRegExp = /schemas\/(?<id>.+)-/;
-    const { id }   = IDRegExp.exec(schema.id).groups;
+    const IDRegExp = /schemas\/(?<$id>.+)-/;
+    const { $id }   = IDRegExp.exec(schema.$id).groups;
 
     // Remove version number from ID for local testing purposes
-    schema.id = schema.id.replace(/-.+?(\.json)/, `$1`);
+    schema.$id = schema.$id.replace(/-.+?(\.json)/, `$1`);
 
     try {
-      ajv.addSchema(schema, id);
+      ajv.addSchema(schema, $id);
     } catch (e) {
-      console.error(`${id} schema is invalid.`);
+      console.error(`${$id} schema is invalid.`);
       console.error(e);
     }
 
