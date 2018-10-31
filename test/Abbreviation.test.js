eaf75773ed2a095cbc3bd45504df3d4cdd342be9
Daniel W. Hieber <dwhieb@gmail.com>
2018-10-31 15:41:17 -0500

    fix example data
    * fix example data
    * allow Orthography.graphemes to be an empty array
    * remove unnecessary tests

--- test/Abbreviation.test.js
+++ test/Abbreviation.test.js
@@ -5,20 +5,11 @@ const { AJV } = require(`./utilities`);
 let ajv;
 let validate;
 
-// VALID SAMPLE DATA
-const data = `chiti`;
-
 describe(`Abbreviation`, () => {
 
   beforeAll(async function setup() {
     ajv = await AJV();
     validate = d => ajv.validate(`Abbreviation`, d);
-  });
-
-  it(`validates`, () => {
-    const valid = validate(data);
-    if (valid) expect(valid).toBe(true);
-    else fail(ajv.errorsText());
   });
 
   it(`invalidates`, () => {
---
+++ test/schemas.test.js
@@ -0,0 +1,40 @@
+/**
+ * This test checks that all the example data in each schema is valid
+ */
+
+// IMPORTS
+const {
+  AJV,
+  getSchemas,
+} = require(`./utilities`);
+
+describe(`schemas`, () => {
+
+  let ajv;
+  let schemas;
+
+  beforeAll(async () => {
+    ajv     = await AJV();
+    schemas = await getSchemas();
+  });
+
+  it(`validate against example data`, () => {
+
+    schemas.forEach(schema => {
+
+      const IDRegExp     = /schemas\/(?<schemaID>.+).json/;
+      const { schemaID } = IDRegExp.exec(schema.$id).groups;
+
+      const validate = data => ajv.validate(schemaID, data);
+
+      schema.examples.forEach(ex => {
+        const valid = validate(ex);
+        if (valid) expect(valid).toBe(true);
+        else fail(`${schemaID}: ${ajv.errorsText()}`);
+      });
+
+    });
+
+  });
+
+});
