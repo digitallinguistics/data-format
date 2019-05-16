9eb2d5eb748788c26d19c10c1a3a832bf4b67176
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2019-05-15 22:44:28 -0500

    Update ajv.js
    Rework getGeoJSONSchema()
    Add GeoJSON.json requirement
    Update getGeoJSONSchema() description

--- test/utilities/ajv.js
+++ test/utilities/ajv.js
@@ -1,7 +1,7 @@
 // IMPORTS
 const AJV        = require(`ajv`);
 const getSchemas = require(`./getSchemas`);
-const http       = require(`http`);
+const GeoJSON    = require(`./GeoJSON.json`);
 
 // SETUP
 const ajv = new AJV({ extendRefs: true });
@@ -12,25 +12,13 @@ let schemasLoaded = false;
 // METHODS
 
 /**
- * Retrieves the GeoJSON schema from an external URL and returns it as a JSON object
+ * Retrieves the GeoJSON schema from the GeoJSON.json file
  * @return {Promise}
  */
 function getGeoJSONSchema() {
   return new Promise(resolve => {
 
-    const GeoJSONURL = `http://geojson.org/schema/GeoJSON.json`;
-
-    http.get(GeoJSONURL, async res => {
-
-      let text = ``;
-
-      for await (const chunk of res) {
-        text += chunk;
-      }
-
-      resolve(JSON.parse(text));
-
-    });
+    resolve(GeoJSON);
 
   });
 }
@@ -54,7 +42,6 @@ async function loadSchemas() {
 
     // Remove version number from ID for local testing purposes
     schema.$id = schema.$id.replace(/-.+?(\.json)/, `$1`);
-
     try {
       ajv.addSchema(schema, $id);
     } catch (e) {
