1f7d312efd1e05381284457276350e718622b52e
Danny Hieber <dwhieb@gmail.com>
2019-09-23 20:56:11 -0500

    use u flag with regular expressions

--- test/utilities/ajv.js
+++ test/utilities/ajv.js
@@ -37,11 +37,12 @@ async function loadSchemas() {
   for (const [, schema] of schemas) {
 
     // Extract the ID from the "id" property of the schema
-    const IDRegExp = /\.io\/(?<$id>.+)-/;
-    const { $id }   = IDRegExp.exec(schema.$id).groups;
+    const IDRegExp = /\.io\/(?<$id>.+)-/u;
+    const { $id }  = IDRegExp.exec(schema.$id).groups;
 
     // Remove version number from ID for local testing purposes
-    schema.$id = schema.$id.replace(/-.+?(\.json)/, `$1`);
+    schema.$id = schema.$id.replace(/-.+?(?<ext>\.json)/u, `$<ext>`);
+
     try {
       ajv.addSchema(schema, $id);
     } catch (e) {
