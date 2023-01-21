bf90de17c9f95132e6f40cd55e72667dc3b9f38e
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-21 16:03:07 -0700

    abandon attempt at creating docs

--- build/docs.js
+++
@@ -1,9 +0,0 @@
-import build from 'wetzel'
-
-import Abbreviation from '../json/Abbreviation.json' assert { type: 'json' }
-
-const markdown = await build({
-  schema: Abbreviation
-})
-
-console.log(markdown);
