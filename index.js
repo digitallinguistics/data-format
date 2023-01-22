19f2d24628b7b5cc11cd701894a3b1cb17f39d7a
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-22 03:07:51 -0700

    remove `Language.glottolog`
    closes #412

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import Abbreviation from './json/Abbreviation.json' assert { type: 'json' }
-import Bundle from './json/Bundle.json' assert { type: 'json' }
 import Access from './json/Access.json' assert { type: 'json' }
 import Address from './json/Address.json' assert { type: 'json' }
 import Annotation from './json/Annotation.json' assert { type: 'json' }
+import Bundle from './json/Bundle.json' assert { type: 'json' }
 import DatabaseReference from './json/DatabaseReference.json' assert { type: 'json' }
-import LanguageTag from './json/LanguageTag.json' assert { type: 'json' }
+import Language from './json/Language.json' assert { type: 'json' }
 import Note from './json/Note.json' assert { type: 'json' }
 import Tags from './json/Tags.json' assert { type: 'json' }
 
@@ -15,7 +15,6 @@ export default {
   Annotation,
   Bundle,
   DatabaseReference,
-  LanguageTag,
   Note,
   Tags,
 }
