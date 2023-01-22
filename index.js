493afdb3fcf0c754bccca6d589d011c84447e4e4
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-22 03:34:57 -0700

    Language.exonyms
    closes #436

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import Annotation from './json/Annotation.json' assert { type: 'json' }
 import Bundle from './json/Bundle.json' assert { type: 'json' }
 import DatabaseReference from './json/DatabaseReference.json' assert { type: 'json' }
 import Language from './json/Language.json' assert { type: 'json' }
+import LanguageTag from './json/LanguageTag.json' assert { type: 'json' }
 import Note from './json/Note.json' assert { type: 'json' }
 import Tags from './json/Tags.json' assert { type: 'json' }
 
@@ -15,6 +16,8 @@ export default {
   Annotation,
   Bundle,
   DatabaseReference,
+  Language,
+  LanguageTag,
   Note,
   Tags,
 }
