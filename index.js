861312fff7929e6ecba151e4a6e4ef5370d72037
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-22 21:38:18 -0700

    Text

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ import CiteItem from './json/CiteItem.json' assert { type: 'json' }
 import DatabaseReference from './json/DatabaseReference.json' assert { type: 'json' }
 import Language from './json/Language.json' assert { type: 'json' }
 import LanguageTag from './json/LanguageTag.json' assert { type: 'json' }
+import Morpheme from './json/Morpheme.json' assert { type: 'json' }
 import MultiLangString from './json/MultiLangString.json' assert { type: 'json' }
 import Note from './json/Note.json' assert { type: 'json' }
+import Person from './json/Person.json' assert { type: 'json' }
 import Tags from './json/Tags.json' assert { type: 'json' }
+import Text from './json/Text.json' assert { type: 'json' }
 import Transcription from './json/Transcription.json' assert { type: 'json' }
+import Utterance from './json/Utterance.json' assert { type: 'json' }
+import Word from './json/Word.json' assert { type: 'json' }
 import WordTemplate from './json/WordTemplate.json' assert { type: 'json' }
 import WordSet from './json/WordSet.json' assert { type: 'json' }
 
@@ -30,10 +35,15 @@ export default {
   DatabaseReference,
   Language,
   LanguageTag,
+  Morpheme,
   MultiLangString,
   Note,
+  Person,
   Tags,
+  Text,
   Transcription,
+  Utterance,
+  Word,
   WordTemplate,
   WordSet,
 }
