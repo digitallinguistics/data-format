e022389ad43a3a06616eb384aa6a371830cb93fd
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-22 19:32:43 -0700

    check for valid examples
    closes #497

--- LICENSE.test.js
+++ LICENSE.test.js
@@ -1,7 +1,7 @@
-import { expect }        from 'chai'
-import { fileURLToPath } from 'url'
-import path              from 'path'
-import { readFile }      from 'fs/promises'
+import { AssertionError } from 'node:assert'
+import { fileURLToPath }  from 'url'
+import path               from 'path'
+import { readFile }       from 'fs/promises'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname  = path.dirname(__filename)
@@ -10,11 +10,21 @@ const yearRegExp = /2013\u2013(?<year>\d{4})/u
 describe(`LICENSE`, function() {
 
   it(`has the correct year`, async function() {
+
     const licensePath = path.join(__dirname, `./LICENSE.md`)
     const text        = await readFile(licensePath, `utf8`)
     const { year }    = yearRegExp.exec(text).groups
     const currentYear = new Date().getFullYear().toString()
-    expect(year).to.equal(currentYear)
+
+    if (year !== currentYear) {
+      throw new AssertionError({
+        actual:   currentYear,
+        expected: year,
+        message:  `Incorrect year in license.`,
+        operator: `=`,
+      })
+    }
+
   })
 
 })
--- index.test.js
+++ index.test.js
@@ -1,19 +1,32 @@
+import { AssertionError } from 'node:assert'
 import schemas   from './index.js'
 import validator from './validator.js'
 
-describe(`schemas`, function() {
-  for (const [type, schema] of Object.entries(schemas)) {
-    it(type, function() {
+describe(`examples`, function() {
 
-      validator.validateSchema(schema)
+  it(`are all valid`, function() {
 
-      if (schema.examples) {
-        for (const example of schema.examples) {
-          const validate = validator.getSchema(schema.$id)
-          validate(example)
+    for (const [type, schema] of Object.entries(schemas)) {
+
+      if (!schema.examples) return
+
+      for (const [i, example] of Object.entries(schema.examples)) {
+
+        const valid = validator.validate(schema, example)
+
+        if (!valid) {
+          throw new AssertionError({
+            actual:   JSON.stringify(validator.errors, null, 2),
+            expected: JSON.stringify(example, null, 2),
+            message:  `The example at index ${ i } for the ${ type } schema is invalid.`,
+            operator: `=`,
+          })
         }
+
       }
 
-    })
-  }
+    }
+
+  })
+
 })
