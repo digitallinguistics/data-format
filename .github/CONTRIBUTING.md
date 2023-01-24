1b4d1f41f7e19df5887e967812b6fd5d1603d637
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-23 18:26:52 -0700

    Phonological Class
    closes #432

--- .github/CONTRIBUTING.md
+++ .github/CONTRIBUTING.md
@@ -5,10 +5,10 @@
 
 ## Schema Conventions
 
-- `description`: Always surround with single quotes, and end with a period (even incomplete sentences). This avoids having to constantly check to see whether a string can go unquoted.
 - `$ref`: Include `title`, `type`, `description` fields for any referenced schema, except when used for `items`. Include the description in the superordinate array property instead. This is useful because the use/interpretation of a schema varies by context. It's also necessary for pointers to DatabaseReference objects.
 - Vertically align property values for better readability.
 - `DatabaseReference`: When referencing the DatabaseReference schema, the title of the referencing schema should be `{Title} (Database Reference(s): {Type})`.
+- The `description` field should end in a period, even when it's not a complete sentence.
 
 ## Third-Party Schemas
 
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import Abbreviation from './json/Abbreviation.json' assert { type: 'json' }
 import Access from './json/Access.json' assert { type: 'json' }
 import Address from './json/Address.json' assert { type: 'json' }
 import Annotation from './json/Annotation.json' assert { type: 'json' }
 import BibItem from './json/BibItem.json' assert { type: 'json' }
 import BibliographicSource from './json/BibliographicSource.json' assert { type: 'json' }
 import Bundle from './json/Bundle.json' assert { type: 'json' }
+import Category from './json/Category.json' assert { type: 'json' }
 import Citation from './json/Citation.json' assert { type: 'json' }
 import CiteItem from './json/CiteItem.json' assert { type: 'json' }
 import DatabaseReference from './json/DatabaseReference.json' assert { type: 'json' }
+import Form from './json/Form.json' assert { type: 'json' }
+import GeoJSON from './json/GeoJSON.json' assert { type: 'json' }
 import Language from './json/Language.json' assert { type: 'json' }
 import LanguageTag from './json/LanguageTag.json' assert { type: 'json' }
 import Location from './json/Location.json' assert { type: 'json' }
@@ -27,16 +29,18 @@ import WordTemplate from './json/WordTemplate.json' assert { type: 'json' }
 import WordSet from './json/WordSet.json' assert { type: 'json' }
 
 export default {
-  Abbreviation,
   Access,
   Address,
   Annotation,
   BibItem,
   BibliographicSource,
   Bundle,
+  Category,
   Citation,
   CiteItem,
   DatabaseReference,
+  Form,
+  GeoJSON,
   Language,
   LanguageTag,
   Location,
